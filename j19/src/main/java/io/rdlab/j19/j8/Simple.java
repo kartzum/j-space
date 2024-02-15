41b4e3167070612677b506c2c22d879bd0cfddd5
kartzum <kartzum@gmail.com>
2024-02-16 00:05:30 +0300

    j8.

--- j19/src/main/java/io/rdlab/j19/j8/Simple.java
+++ j19/src/main/java/io/rdlab/j19/j8/Simple.java
@@ -1,7 +1,34 @@
 package io.rdlab.j19.j8;
 
-public class Simple {
-    public static void main(String[] args) {
+import java.util.Collections;
+import java.util.function.Function;
 
+public class Simple {
+
+    private static class A {
+        interface Operationable {
+            int calculate(int x, int y);
+        }
+
+        static void calc(Function<Integer, Integer> f) {
+            f.apply(0);
+        }
+
+        static void test() {
+            Operationable operation = (x, y) -> x + y;
+            int result = operation.calculate(10, 20);
+            System.out.println(result); //30
+
+            calc((n) -> n + 1);
+
+            Function<Integer, Integer> f = (a) -> a + 1;
+            System.out.println(f.apply(1));
+
+            Collections.sort(Collections.singletonList(2), (a, b) -> a.compareTo(b));
+        }
+    }
+
+    public static void main(String[] args) {
+        A.test();
     }
 }
